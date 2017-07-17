ce487f70e30a8de7f64f18ed1c02b4464ede8455
BAH\577859 <Coxiii_Charles@bah.com>
2017-07-17 16:34:00 -0400

    initial commit
    initial commit

---
+++ Cloud Build Test/Assets/TestScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+	float t;
+	   int blendShapeCount;
+       SkinnedMeshRenderer skinnedMeshRenderer;
+       Mesh skinnedMesh;
+       float blendOne = 0f;
+       float blendTwo = 0f;
+       public float blendSpeed = 1f;
+       bool blendOneFinished = false;
+ 
+       void Awake ()
+       {
+          skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer> ();
+          skinnedMesh = GetComponent<SkinnedMeshRenderer> ().sharedMesh;
+       }
+ 
+       void Start ()
+       {
+          blendShapeCount = skinnedMesh.blendShapeCount; 
+       }
+ 
+       void Update ()
+       {
+ 
+                 if (blendOne < 100f) {
+                    skinnedMeshRenderer.SetBlendShapeWeight (0, blendOne);
+                    blendOne += blendSpeed;
+                 } else {
+                    blendOne = 0;
+                 }
+       }
+}
